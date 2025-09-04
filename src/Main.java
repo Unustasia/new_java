97944816570f3eb595d7790af571945bc0f9e518
Anastasia Boltunova <anastaciaboltunova@gmail.com>
2025-09-04 16:39:53 +0500

    puppy and конструктор ---

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello");
 
-        }
+public class Main{
+
+    int puppyAge;
+
+    public Main(String name){
+        // Это конструктор и у него один параметр, name.
+        // Это самое тупое, зачем... если есть метод, зачем делать какой-то
+        // конструктор
+        System.out.println("Имя:" + name );
     }
+    public void setAge( int age ){
+        puppyAge = age;
+    }
+
+    public int getAge( ){
+        System.out.println("Возраст щенка:" + puppyAge );
+        return puppyAge;
+    }
+    public static void main(String []args){
+        /* Создание объекта. */
+        Main myPuppy = new Main( "Рыжик" );
+
+        /* Вызов метод класса, чтобы установить возраст щенка. */
+        myPuppy.setAge( 2 );
+
+        /* Вызов другого метода класса, чтобы получить возраст щенка. */
+        myPuppy.getAge( );
+
+        /* Получение переменной экземпляра класса. */
+        System.out.println("Значение переменной:" + myPuppy.puppyAge );
+    }
+}
